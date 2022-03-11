7f409f8cdf18d413ffc30d3f4b67867e5610479b
Jevier Izza Maulana <izzajevier@gmail.com>
2022-03-12 00:18:08 +0700

    prototype counter timer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float timeValue = 90;
+    public Text timerText;
+
+    bool checkColide;
+
+    void Update()
+    {
+        if(checkColide == false)
+        {
+            DisplayTime(timeValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            checkColide = true;
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        else if (timeToDisplay > 0)
+        {
+            timeToDisplay += 1;
+        }
+
+        float minute = Mathf.FloorToInt(timeToDisplay / 60);
+        float second = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minute, second);
+    }
+}
